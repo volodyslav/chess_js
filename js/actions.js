1eacd910a5b3a8f33d219c51302e2be4eded0331
Vladyslav <vladver1211@gmail.com>
2024-10-22 16:29:10 +0300

    add: white pawn movement (continue); add: check can move or checked

--- js/actions.js
+++ js/actions.js
@@ -6,17 +6,36 @@ function chooseFigure(img){
         const x = event.clientX; // Mouse coordinates
         const y = event.clientY;
         const rect = img.getBoundingClientRect(); // Get the bounds of the image
-        if (x >= rect.left && x <= rect.right &&
-            y >= rect.top && y <= rect.bottom){ // Check if mouse inside the image
+        const canCheck = canMoveCheck(img); // Check if the figure can be checked
+        if (canCheck && (x >= rect.left && x <= rect.right &&
+            y >= rect.top && y <= rect.bottom)){ // Check if mouse inside the image
             img.classList.add("check-image"); // Change color in choosen figure
             moveFigure(img);
         }   
     });
 }
 
+function canMoveCheck(img){
+    // Check if the figure can move or be checked
+    const imgLeft = parseInt(img.style.left);
+    const imgTop = parseInt(img.style.top);
+    //console.log(`Image left: ${imgLeft} top: ${imgTop}`)
+    const imgPositionLeft = imgLeft / squareWidth; // Get the position of the figure in array structure (0, 0) or (1, 4)
+    const imgPositionTop = imgTop / squareHeight;
+    //console.log(`Image position: ${imgPositionLeft} ${imgPositionTop} `);
+    // Check if the figure can move or be checked
+    if (currentColorArray.includes(boardPosition[imgPositionTop][imgPositionLeft])){
+        //console.log(`Figure ${boardPosition[imgPositionTop][imgPositionLeft]}`)
+        return true;
+    }else {
+        return false;
+    }
+}
+
 function moveFigure(img){
-    // Check if the figure 
-    if (img.classList.contains("check-image")){
+    // Check if the figure can move
+    const canMove = canMoveCheck(img);
+    if ((img.classList.contains("check-image")) && canMove){
         // Check if this figure is checked
         const imgLeft = parseInt(img.style.left);
         const imgTop = parseInt(img.style.top);
@@ -24,13 +43,35 @@ function moveFigure(img){
         const imgPositionLeft = imgLeft / squareWidth; // Get the position of the figure in array structure (0, 0) or (1, 4)
         const imgPositionTop = imgTop / squareHeight;
         console.log(`Image position: ${imgPositionLeft} ${imgPositionTop} `);
-        
+        console.log(`Figure ${boardPosition[imgPositionTop][imgPositionLeft]}`)
 
-        if (boardPosition[imgPositionTop][imgPositionLeft] in currentColorArray){
-            console.log(`Figure ${boardPosition[imgPositionTop][imgPositionLeft]}`)
+        const rect = img.getBoundingClientRect();
+        
+        
+        switch (boardPosition[imgPositionTop][imgPositionLeft]){
+            case 11:
+                // Move pawn
+                if (((boardPosition[imgPositionTop - 1][imgPositionLeft]) === 0) ){
+                    // Move forward
+                    board.addEventListener("mousedown", (event) => {
+                        const x = event.clientX; // Mouse coordinates
+                        const y = event.clientY;
+                        console.log(`x: ${x} y: ${y}`);  
+                        console.log(`left: ${rect.left} top: ${rect.top}`)
+                        if (x >= rect.left && x <= rect.right &&
+                            y >= rect.top - squareHeight && y <= rect.bottom - squareHeight){  
+                                img.style.top = (imgTop - squareHeight) + 'px';
+                                boardPosition[imgPositionTop - 1][imgPositionLeft] = 11;
+                                boardPosition[imgPositionTop][imgPositionLeft] = 0;
+                                
+                                console.log(`Board after ${boardPosition}`);
+                        }
+                    })
+                    
+                }
+                break;
+            default:
+                break;
         }
-
-        
     }
-
 }
