a3109c45a43af1ef6bd610004863f7f3f6599c6b
Vladyslav <vladver1211@gmail.com>
2024-11-07 15:38:14 +0200

    add: king move to rook check if rook has first move

--- js/pieces/king.js
+++ js/pieces/king.js
@@ -1,31 +1,44 @@
 function moveKing(imgPositionTop, imgPositionLeft, img, colorImgNumber){
     const moveRightToRook = colorImgNumber === 16 ? 2 : 3; // move right to rook
     const moveLeftToRook = colorImgNumber === 16 ? 3 : 2;
+    const rookColorImagePath = colorImgNumber === 16 ? whiteRook : blackRook; // Color of rook image
 
+    const imageRightRook = document.querySelector(
+        `#board img[style*="top: ${imgPositionTop * squareHeight}px;"][style*="left: ${(imgPositionLeft + moveRightToRook + 1) * squareWidth}px;"]` // Finds coordinates of rook image on board
+    );
+    const imageLeftRook = document.querySelector(
+        `#board img[style*="top: ${imgPositionTop * squareHeight}px;"][style*="left: ${(imgPositionLeft - (moveLeftToRook + 1)) * squareWidth}px;"]` // Finds coordinates of rook image on board
+    );
+    // For king rook combination
     if (img.classList.contains("first-move")){
-        for (let i = 1; i <= moveRightToRook; i++) {
-            const imgLeft = imgPositionLeft + i;
-            if (0 <= imgLeft && imgLeft < 8){
-                if (boardPosition[imgPositionTop][imgLeft] === 0){
-                    console.log(imgLeft, imgPositionTop)
-                    drawCirclesKingRook(imgLeft, imgPositionTop); // utils.js
-                }else if (boardPosition[imgPositionTop][imgLeft] > 0){
-                    break;
+        if (imageRightRook && imageRightRook.getAttribute("src") === rookColorImagePath && imageRightRook.classList.contains("first-move")){ // Check if scr of the rook equals to the path of a rook image
+            for (let i = 1; i <= moveRightToRook; i++) {
+                const imgLeft = imgPositionLeft + i;
+                if (0 <= imgLeft && imgLeft < 8){
+                    if (boardPosition[imgPositionTop][imgLeft] === 0){
+                        console.log(imgLeft, imgPositionTop)
+                        drawCirclesKingRook(imgLeft, imgPositionTop); // utils.js
+                    }else if (boardPosition[imgPositionTop][imgLeft] > 0){
+                        break;
+                    }
+                }
+            } 
+        }
+
+        if (imageLeftRook && imageLeftRook.getAttribute("src") === rookColorImagePath && imageLeftRook.classList.contains("first-move")){
+            for (let i = 1; i <= moveLeftToRook; i++) { // Left from king 
+                const imgLeft = imgPositionLeft - i;
+                if (0 <= imgLeft && imgLeft < 8){
+                    if (boardPosition[imgPositionTop][imgLeft] === 0){
+                        console.log(imgLeft, imgPositionTop)
+                        drawCirclesKingRook(imgLeft, imgPositionTop); // utils.js
+                    }else if (boardPosition[imgPositionTop][imgLeft] > 0){
+                        break;
+                    }
                 }
             }
         } 
-        for (let i = 1; i <= moveLeftToRook; i++) { // Left from king 
-            const imgLeft = imgPositionLeft - i;
-            if (0 <= imgLeft && imgLeft < 8){
-                if (boardPosition[imgPositionTop][imgLeft] === 0){
-                    console.log(imgLeft, imgPositionTop)
-                    drawCirclesKingRook(imgLeft, imgPositionTop); // utils.js
-                }else if (boardPosition[imgPositionTop][imgLeft] > 0){
-                    break;
-                }
-            }
-        } 
-    }// For king rook combination
+    }
     
     
     // Horizontal and vertical circles
@@ -44,7 +57,6 @@ function moveKing(imgPositionTop, imgPositionLeft, img, colorImgNumber){
         const movX = event.clientX - board.getBoundingClientRect().left; // Get offset of the board from the mouse position
         const movY = event.clientY - board.getBoundingClientRect().top;
         
-        console.log(`x: ${movX} y: ${movY}`);  
         const movePositionY = Math.floor(movY / squareHeight); // Get position on the board from the mouse position
         const movePositionX = Math.floor(movX / squareWidth); // Get position on the board from the mouse position
        
@@ -52,7 +64,6 @@ function moveKing(imgPositionTop, imgPositionLeft, img, colorImgNumber){
         const conditionY = Math.abs(movePositionY - imgPositionTop) === 1  && imgPositionLeft === movePositionX;
         const conditionDiagonal = (Math.abs(movePositionX - imgPositionLeft) === 1 && Math.abs(movePositionY - imgPositionTop) === 1) && (imgPositionTop !== movePositionY && imgPositionLeft !== movePositionX); // Check diagonal movement
 
-        console.log(`can x: ${conditionX} can y: ${conditionY}`); 
 
         if ((conditionX || conditionY || conditionDiagonal) && boardPosition[movePositionY][movePositionX] === 0 && (img.classList.contains("check-image"))) {
             img.classList.add("chess-piece-animation"); // Move left animation piece
